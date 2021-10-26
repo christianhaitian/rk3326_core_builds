74ff3798963a30e754aaa10f3acf4f51d71d5c4f
Christian_Haitian <mckman@comcast.net>
2021-10-26 22:27:25 +0000

    Add scripts for building caprice32,genesis-plus-gx and sameduck and remove duplicate repo update script

---
+++ scripts/genesis-plus-gx.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+cur_wd="$PWD"
+bitness="$(getconf LONG_BIT)"
+
+	# Libretro genesis-plus-gx build
+	if [[ "$var" == "genesis-plus-gx" || "$var" == "all" ]] && [[ "$bitness" == "64" ]]; then
+	 cd $cur_wd
+	  if [ ! -d "genesis-plus-gx-libretro/" ]; then
+		git clone https://github.com/libretro/Genesis-Plus-GX.git
+		if [[ $? != "0" ]]; then
+		  echo " "
+		  echo "There was an error while cloning the libretro git.  Is Internet active or did the git location change?  Stopping here."
+		  exit 1
+		 fi
+		cp patches/genesis-plus-gx-patch* Genesis-Plus-GX/.
+	  fi
+
+	 cd Genesis-Plus-GX/
+	 
+	 genesis_plus_gx_patches=$(find *.patch)
+	 
+	 if [[ ! -z "$genesis_plus_gx_patches" ]]; then
+	  for patching in genesis-plus-gx-patch*
+	  do
+		   patch -Np1 < "$patching"
+		   if [[ $? != "0" ]]; then
+			echo " "
+			echo "There was an error while applying $patching.  Stopping here."
+			exit 1
+		   fi
+		   rm "$patching" 
+	  done
+	 fi
+
+	  make clean
+	  make -f Makefile.libretro -j$(nproc)
+
+	  if [[ $? != "0" ]]; then
+		echo " "
+		echo "There was an error while building the newest lr-genesis-plus-gx core.  Stopping here."
+		exit 1
+	  fi
+
+	  strip genesis_plus_gx_libretro.so
+
+	  if [ ! -d "../cores64/" ]; then
+		mkdir -v ../cores64
+	  fi
+
+	  cp genesis_plus_gx_libretro.so ../cores64/.
+
+	  gitcommit=$(git show | grep commit | cut -c -14 | cut -c 8-)
+	  echo $gitcommit > ../cores$(getconf LONG_BIT)/genesis_plus_gx_libretro.so.commit
+
+	  echo " "
+	  echo "genesis_plus_gx_libretro.so has been created and has been placed in the rk3326_core_builds/cores64 subfolder"
+	fi
