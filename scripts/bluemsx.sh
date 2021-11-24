19a430309995ef2f715243020695281829864402
Christian_Haitian <mckman@comcast.net>
2021-11-24 03:35:42 +0000

    Add lr-bluemsx and add additional patch for openmsx savestate directory location

---
+++ scripts/bluemsx.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+cur_wd="$PWD"
+bitness="$(getconf LONG_BIT)"
+
+	# Libretro blueMSX build
+	if [[ "$var" == "bluemsx" || "$var" == "all" ]] && [[ "$bitness" == "64" ]]; then
+	 cd $cur_wd
+	  if [ ! -d "blueMSX-libretro/" ]; then
+		git clone https://github.com/libretro/blueMSX-libretro.git
+		if [[ $? != "0" ]]; then
+		  echo " "
+		  echo "There was an error while cloning the libretro git.  Is Internet active or did the git location change?  Stopping here."
+		  exit 1
+		 fi
+		cp patches/bluemsx-patch* blueMSX-libretro/.
+	  fi
+
+	 cd blueMSX-libretro/
+	 
+	 bluemsx_patches=$(find *.patch)
+	 
+	 if [[ ! -z "$bluemsx_patches" ]]; then
+	  for patching in bluemsx-patch*
+	  do
+		   patch -Np1 < "$patching"
+		   if [[ $? != "0" ]]; then
+			echo " "
+			echo "There was an error while applying $patching.  Stopping here."
+			exit 1
+		   fi
+		   rm "$patching" 
+	  done
+	 fi
+
+	  make clean
+	  make -f Makefile.libretro -j$(nproc)
+
+	  if [[ $? != "0" ]]; then
+		echo " "
+		echo "There was an error while building the newest lr-blueMSX core.  Stopping here."
+		exit 1
+	  fi
+
+	  strip bluemsx_libretro.so
+
+	  if [ ! -d "../cores64/" ]; then
+		mkdir -v ../cores64
+	  fi
+
+	  cp bluemsx_libretro.so ../cores64/.
+
+	  gitcommit=$(git show | grep commit | cut -c -14 | cut -c 8-)
+	  echo $gitcommit > ../cores$(getconf LONG_BIT)/blueMSX_libretro.so.commit
+
+	  echo " "
+	  echo "bluemsx_libretro.so has been created and has been placed in the rk3326_core_builds/cores64 subfolder"
+	fi
