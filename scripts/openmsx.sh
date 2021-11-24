1209a4fae1ea9abc3927acefde93173f2855661a
Christian_Haitian <mckman@comcast.net>
2021-11-24 01:21:14 +0000

    Add openmsx standalone build and patches and update lib install check and install process for ppsspp, scummvm, and solarus

---
+++ scripts/openmsx.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+cur_wd="$PWD"
+bitness="$(getconf LONG_BIT)"
+
+	# openmsx Standalone build
+	if [[ "$var" == "openmsx" ]] && [[ "$bitness" == "64" ]]; then
+	 cd $cur_wd
+
+	  # Now we'll start the clone and build of openmsx
+	  if [ ! -d "openmsx/" ]; then
+		git clone https://github.com/openMSX/openMSX.git -b gles2
+
+		if [[ $? != "0" ]]; then
+		  echo " "
+		  echo "There was an error while cloning the openmsx standalone git.  Is Internet active or did the git location change?  Stopping here."
+		  exit 1
+		fi
+		cp patches/openmsx-patch* openMSX/.
+	  else
+		echo " "
+		echo "A openmsx subfolder already exists.  Stopping here to not impact anything in the folder that may be needed.  If not needed, please remove the openmsx folder and rerun this script."
+		echo " "
+		exit 1
+	  fi
+
+	 # Ensure dependencies are installed and available
+     neededlibs=( tcl-dev libpng-dev zlib1g-dev libsdl2-ttf-dev libglew-dev libogg-dev libvorbis-dev libtheora-dev libasound2-dev )
+     updateapt="N"
+     for libs in "${neededlibs[@]}"
+     do
+          dpkg -s "${libs}" &>/dev/null
+          if [[ $? != "0" ]]; then
+           if [[ "$updateapt" == "N" ]]; then
+            apt-get -y update
+            updateapt="Y"
+           fi
+           apt-get -y install "${libs}"
+           if [[ $? != "0" ]]; then
+            echo " "
+            echo "Could not install needed library ${libs}.  Stopping here so this can be reviewed."
+            exit 1
+           fi
+          fi
+     done
+
+	 cd openMSX
+	 
+	 openmsx_patches=$(find *.patch)
+	 
+	 if [[ ! -z "$openmsx_patches" ]]; then
+	  for patching in openmsx-patch*
+	  do
+		   patch -Np1 < "$patching"
+		   if [[ $? != "0" ]]; then
+			echo " "
+			echo "There was an error while applying $patching.  Stopping here."
+			exit 1
+		   fi
+		   rm "$patching" 
+	  done
+	 fi
+
+	  make -j$(nproc)
+
+	  if [[ $? != "0" ]]; then
+		echo " "
+		echo "There was an error while building the newest openmsx standalone.  Stopping here."
+		exit 1
+	  fi
+
+	  strip derived/aarch64-linux-opt/bin/openmsx
+
+	  if [ ! -d "../openmsx$bitness/" ]; then
+		mkdir -v ../openmsx$bitness
+	  fi
+
+	  cp derived/aarch64-linux-opt/bin/openmsx ../openmsx$bitness/.
+	  tar -zchvf ../openmsx$bitness/openmsx_pkg_$(git rev-parse HEAD | cut -c -7).tar.gz share/ derived/aarch64-linux-opt/bin/openmsx
+
+	  echo " "
+	  echo "The openmsx executable and openmsx_pkg_$(git rev-parse HEAD | cut -c -7).tar.gz package has been created and has been placed in the rk3326_core_builds/openmsx$bitness subfolder"
+
+	fi
