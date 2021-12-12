0c358493f688f14688bd3fcd1ac91df36cb6427e
Christian_Haitian <mckman@comcast.net>
2021-12-12 02:03:34 +0000

    Add nestopia and tic-80 builds

---
+++ scripts/nestopia.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+cur_wd="$PWD"
+bitness="$(getconf LONG_BIT)"
+
+	# Libretro nestopia build
+	if [[ "$var" == "nestopia" || "$var" == "all" ]] && [[ "$bitness" == "64" ]]; then
+	 cd $cur_wd
+	  if [ ! -d "nestopia/" ]; then
+		git clone https://github.com/libretro/nestopia.git
+		if [[ $? != "0" ]]; then
+		  echo " "
+		  echo "There was an error while cloning the libretro git.  Is Internet active or did the git location change?  Stopping here."
+		  exit 1
+		 fi
+		cp patches/nestopia-patch* nestopia/.
+	  fi
+
+	 cd nestopia/
+	 
+	 nestopia_patches=$(find *.patch)
+	 
+	 if [[ ! -z "$nestopia_patches" ]]; then
+	  for patching in nestopia-patch*
+	  do
+		   patch -Np1 < "$patching"
+		   if [[ $? != "0" ]]; then
+			echo " "
+			echo "There was an error while applying $patching.  Stopping here."
+			exit 1
+		   fi
+		   rm "$patching" 
+	  done
+	 fi
+
+	  make clean
+	  make -C libretro -j$(nproc)
+
+	  if [[ $? != "0" ]]; then
+		echo " "
+		echo "There was an error while building the newest lr-nestopia core.  Stopping here."
+		exit 1
+	  fi
+
+	  strip libretro/nestopia_libretro.so
+
+	  if [ ! -d "../cores64/" ]; then
+		mkdir -v ../cores64
+	  fi
+
+	  cp libretro/nestopia_libretro.so ../cores64/.
+
+	  gitcommit=$(git show | grep commit | cut -c -14 | cut -c 8-)
+	  echo $gitcommit > ../cores$(getconf LONG_BIT)/nestopia_libretro.so.commit
+
+	  echo " "
+	  echo "nestopia_libretro.so has been created and has been placed in the rk3326_core_builds/cores64 subfolder"
+	fi
