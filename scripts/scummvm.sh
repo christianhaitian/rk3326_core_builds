77af566676b5e04744d5e9d466db36926cd0fabb
christianhaitian <mckman@comcast.net>
2022-05-16 22:16:31 -0400

    Update scummvm.sh

--- scripts/scummvm.sh
+++ scripts/scummvm.sh
@@ -103,20 +103,27 @@ minfluidsynthverneeded="3"
 	  cp backends/vkeybd/packs/*.zip extra/.
 	  cp dists/pred.dic extra.
 	  cp dists/engine-data/*.dat extra/.
+	  cp dists/engine-data/grim-patch.lab extra/.
+	  cp dists/engine-data/monkey4-patch.lab extra/.
+	  cp dists/engine-data/queen.tbl extra/.
+	  cp dists/engine-data/sky.cpt extra/.
+	  cp dists/engine-data/wintermute.zip extra/.
+	  cp dists/engine-data/xeen.ccs extra/.
 	  cp gui/themes/*.zip themes/.
 	  cp gui/themes/translations.dat themes/. 
 	  echo "19000000030000000300000002030000,gameforce_gamepad,leftstick:b14,rightx:a3,leftshoulder:b4,start:b9,lefty:a0,dpup:b10,righty:a2,a:b1,b:b0,guide:b16,dpdown:b11,rightshoulder:b5,righttrigger:b7,rightstick:b15,dpright:b13,x:b2,back:b8,leftx:a1,y:b3,dpleft:b12,lefttrigger:b6,platform:Linux,
 190000004b4800000010000000010000,GO-Advance Gamepad,a:b1,b:b0,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,dpdown:b7,dpleft:b8,dpright:b9,dpup:b6,leftx:a0,lefty:a1,leftstick:b10,guide:b12,lefttrigger:b11,rightstick:b13,righttrigger:b14,start:b15,platform:Linux,
 190000004b4800000010000001010000,GO-Advance Gamepad (rev 1.1),a:b1,b:b0,x:b2,y:b3,leftshoulder:b4,rightshoulder:b5,dpdown:b9,dpleft:b10,dpright:b11,dpup:b8,leftx:a0,lefty:a1,guide:b12,leftstick:b14,lefttrigger:b13,rightstick:b15,righttrigger:b16,start:b17,platform:Linux,
 190000004b4800000011000000010000,GO-Super Gamepad,platform:Linux,x:b2,a:b1,b:b0,y:b3,back:b12,guide:b16,start:b13,dpleft:b10,dpdown:b9,dpright:b11,dpup:b8,leftshoulder:b4,lefttrigger:b6,rightshoulder:b5,righttrigger:b7,leftstick:b14,rightstick:b17,leftx:a0,lefty:a1,rightx:a2,righty:a3,platform:Linux,
-03000000091200000031000011010000,OpenSimHardware OSH PB Controller,a:b1,b:b0,x:b3,y:b2,leftshoulder:b4,rightshoulder:b5,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,leftx:a0~,lefty:a1~,guide:b10,leftstick:b8,lefttrigger:b13,rightstick:b9,back:b7,start:b6,rightx:a2,righty:a3,righttrigger:b11,platform:Linux," > extra/gamecontrollerdb.txt
+03000000091200000031000011010000,OpenSimHardware OSH PB Controller,a:b1,b:b0,x:b3,y:b2,leftshoulder:b4,rightshoulder:b5,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,leftx:a0~,lefty:a1~,guide:b10,leftstick:b8,lefttrigger:b13,rightstick:b9,back:b7,start:b6,rightx:a2,righty:a3,righttrigger:b11,platform:Linux,
+190000004b4800000111000000010000,retrogame_joypad,a:b1,b:b0,x:b2,y:b3,back:b8,start:b9,rightstick:b12,leftstick:b11,dpleft:b15,dpdown:b14,dpright:b16,dpup:b13,leftshoulder:b4,lefttrigger:b6,rightshoulder:b5,righttrigger:b7,leftx:a0,lefty:a1,rightx:a2,righty:a3,platform:Linux," > extra/gamecontrollerdb.txt
 
 	  if [ ! -d "../scummvm$bitness/" ]; then
 		mkdir -v ../scummvm$bitness
 	  fi
 
 	  cp scummvm ../scummvm$bitness/.
-	  tar -zchvf ../scummvm$bitness/scummvm_pkg_$(git rev-parse HEAD | cut -c -7).tar.gz extra/ themes/ scummvm AUTHORS COPYING NEWS.md README.md
+	  tar -zchvf ../scummvm$bitness/scummvm_pkg_$(git rev-parse HEAD | cut -c -7).tar.gz extra/ themes/ LICENSES/ scummvm AUTHORS COPYING NEWS.md README.md
 
 	  echo " "
 	  echo "scummvm has been created and has been placed in the rk3326_core_builds/scummvm$bitness subfolder"
