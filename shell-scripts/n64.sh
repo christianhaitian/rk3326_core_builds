fde0347ba8127e286a2637640317861e381cc990
christianhaitian <mckman@comcast.net>
2022-07-04 19:18:47 -0400

    Update n64.sh shell script and cleanup

--- shell-scripts/n64.sh
+++ shell-scripts/n64.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-if [[ -e "/dev/input/by-path/platform-ff300000.usb-usb-0:1.2:1.0-event-joystick" ]]; then
-	  RES="480x320"
-      if [ -f "/boot/rk3326-rg351v-linux.dtb" ]; then
-        RES="640x480"
-      fi
-elif [[ -e "/dev/input/by-path/platform-odroidgo2-joypad-event-joystick" ]]; then
-	  RES="480x320"
-elif [[ -e "/dev/input/by-path/platform-odroidgo3-joypad-event-joystick" ]]; then
-	  RES="640x480"
-elif [[ -e "/dev/input/by-path/platform-gameforce-gamepad-event-joystick" ]]; then
-	  RES="640x480"
-else
-	  RES="640x480"
-fi
+xres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f1)"
+yres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f2)"
+ricewidthhack=$(((yres * 4) / 3))
 
 if [[ $1 == "standalone-Rice" ]]; then
-  /opt/mupen64plus/mupen64plus --resolution $RES --plugindir /opt/mupen64plus --gfx mupen64plus-video-rice.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$2"
+  if [[ $2 == "Widescreen_Aspect" ]]; then
+    /opt/mupen64plus/mupen64plus --resolution "${xres}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-rice.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$3"
+  else
+    /opt/mupen64plus/mupen64plus --resolution "${ricewidthhack}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-rice.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$3"
+  fi
 elif [[ $1 == "standalone-Glide64mk2" ]]; then
-  /opt/mupen64plus/mupen64plus --resolution $RES --plugindir /opt/mupen64plus --gfx mupen64plus-video-glide64mk2.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$2"
+  if [[ $2 == "Widescreen_Aspect" ]]; then
+    /opt/mupen64plus/mupen64plus --set Video-Glide64mk2[aspect]=1 --resolution "${xres}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-glide64mk2.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$3"
+  else
+    /opt/mupen64plus/mupen64plus --set Video-Glide64mk2[aspect]=-1 --resolution "${xres}x${yres}" --plugindir /opt/mupen64plus --gfx mupen64plus-video-glide64mk2.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$3"
+  fi
 else
   /usr/local/bin/"$1" -L /home/ark/.config/"$1"/cores/"$2"_libretro.so "$3"
 fi
+
 
