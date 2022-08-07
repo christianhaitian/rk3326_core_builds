22e41fc75ce613a5e98b56e08e7ab055cadba5e7
christianhaitian <mckman@comcast.net>
2022-08-07 19:29:37 -0400

    Add mali blobs and additional retroarch patches

--- builds.sh
+++ builds.sh
@@ -27,6 +27,47 @@ do
         echo "$var cannot be built in this current ${bitness}bit environment."
         exit 1
       fi
+      if [[ ${bitness} == "32" ]]; then
+        cp mali/armhf/libmali-bifrost-g31-rxp0-gbm.so /usr/lib/arm-linux-gnueabihf/.
+        cd /usr/lib/arm-linux-gnueabihf/
+        whichmali="libmali-bifrost-g31-rxp0-gbm.so"
+      else
+        cp mali/aarch64/libmali-bifrost-g31-rxp0-gbm.so /usr/lib/aarch64-linux-gnu/.
+        cd /usr/lib/aarch64-linux-gnu/
+        whichmali="libmali-bifrost-g31-rxp0-gbm.so"
+      fi
+      rm libMali.so
+      rm libEGL.so*
+      rm libGLES*
+      rm libgbm.so*
+      rm libmali.so*
+      rm libMali*
+      rm libOpenCL*
+      rm libwayland-egl*
+      ln -sf ${whichmali} libMali.so
+      ln -sf libMali.so libEGL.so
+      ln -sf libMali.so libGLES_CM.so
+      ln -sf libMali.so libGLES_CM.so.1
+      ln -sf libMali.so libGLESv1_CM.so
+      ln -sf libMali.so libGLESv1_CM.so.1
+      ln -sf libMali.so libGLESv1_CM.so.1.1.0
+      ln -sf libMali.so libGLESv2.so
+      ln -sf libMali.so libGLESv2.so.2
+      ln -sf libMali.so libGLESv2.so.2.0.0
+      ln -sf libMali.so libGLESv2.so.2.1.0
+      ln -sf libMali.so libGLESv3.so
+      ln -sf libMali.so libGLESv3.so.3
+      ln -sf libMali.so libgbm.so
+      ln -sf libMali.so libgbm.so.1
+      ln -sf libMali.so libgbm.so.1.0.0
+      ln -sf libMali.so libmali.so
+      ln -sf libMali.so libmali.so.1
+      ln -sf libMali.so libMaliOpenCL.so
+      ln -sf libMali.so libOpenCL.so
+      ln -sf libMali.so libwayland-egl.so
+      ln -sf libMali.so libwayland-egl.so.1
+      ln -sf libMali.so libwayland-egl.so.1.0.0
+      cd "${cur_wd}"
       source scripts/"$var".sh
   else
        cur_var="$var"
