d869ba89965cfafca67828b459b3d00970bb818a
Christian_Haitian <mckman@comcast.net>
2022-08-26 04:21:04 +0000

    Update descriptions in libretro fuse, game&watch, mame2000, and tic-80 scripts

--- scripts/fuse.sh
+++ scripts/fuse.sh
@@ -3,7 +3,7 @@
 ##################################################################
 # Created by Christian Haitian for use to easily update          #
 # various standalone emulators, libretro cores, and other        #
-# various programs for the RK3566 platform for various Linux     #
+# various programs for the RK3326 platform for various Linux     #
 # based distributions.                                           #
 # See the LICENSE.md file at the top-level directory of this     #
 # repository.                                                    #
@@ -63,5 +63,5 @@ bitness="$(getconf LONG_BIT)"
 	  echo $gitcommit > ../cores$(getconf LONG_BIT)/fuse_libretro.so.commit
 
 	  echo " "
-	  echo "fuse_libretro.so has been created and has been placed in the rk3566_core_builds/cores64 subfolder"
+	  echo "fuse_libretro.so has been created and has been placed in the rk3326_core_builds/cores64 subfolder"
 	fi
