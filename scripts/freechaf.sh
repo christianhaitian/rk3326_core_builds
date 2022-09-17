3b9299055c7a1796928dd97c334941515a974e5d
Christian_Haitian <mckman@comcast.net>
2022-09-17 19:40:12 +0000

    Add freechaf, race, and beetle vb core builds

---
+++ scripts/freechaf.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+##################################################################
+# Created by Christian Haitian for use to easily update          #
+# various standalone emulators, libretro cores, and other        #
+# various programs for the RK3326 platform for various Linux     #
+# based distributions.                                           #
+# See the LICENSE.md file at the top-level directory of this     #
+# repository.                                                    #
+##################################################################
+
+cur_wd="$PWD"
+bitness="$(getconf LONG_BIT)"
+
+	# Libretro freechaf build
+	if [[ "$var" == "freechaf" || "$var" == "all" ]] && [[ "$bitness" == "64" ]]; then
+	 cd $cur_wd
+	  if [ ! -d "freechaf/" ]; then
+		git clone --recursive https://github.com/libretro/freechaf.git
+		if [[ $? != "0" ]]; then
+		  echo " "
+		  echo "There was an error while cloning the libretro git.  Is Internet active or did the git location change?  Stopping here."
+		  exit 1
+		 fi
+		cp patches/freechaf-patch* freechaf/.
+	  fi
+
+	 cd freechaf/
+	 
+	 freechaf_patches=$(find *.patch)
+	 
+	 if [[ ! -z "$freechaf_patches" ]]; then
+	  for patching in freechaf-patch*
+	  do
+		   patch -Np1 < "$patching"
+		   if [[ $? != "0" ]]; then
+			echo " "
+			echo "There was an error while applying $patching.  Stopping here."
+			exit 1
+		   fi
+		   rm "$patching" 
+	  done
+	 fi
+
+	  make clean
+	  make -j$(nproc)
+
+	  if [[ $? != "0" ]]; then
+		echo " "
+		echo "There was an error while building the newest lr-freechaf core.  Stopping here."
+		exit 1
+	  fi
+
+	  strip freechaf_libretro.so
+
+	  if [ ! -d "../cores64/" ]; then
+		mkdir -v ../cores64
+	  fi
+
+	  cp freechaf_libretro.so ../cores64/.
+
+	  gitcommit=$(git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
+	  echo $gitcommit > ../cores$(getconf LONG_BIT)/freechaf_libretro.so.commit
+
+	  echo " "
+	  echo "freechaf_libretro.so has been created and has been placed in the rk3326_core_builds/cores64 subfolder"
+	fi
