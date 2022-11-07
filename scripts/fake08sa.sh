42cb48068250ba4fa9a8825cad7b30f300dbc5ba
Christian_Haitian <mckman@comcast.net>
2022-11-07 04:55:08 +0000

    Add fake08 libretro core build

--- scripts/fake08sa.sh
+++ scripts/fake08sa.sh
@@ -13,32 +13,32 @@ cur_wd="$PWD"
 bitness="$(getconf LONG_BIT)"
 
 	# fake08 Standalone build
-	if [[ "$var" == "fake08" || "$var" == "all" ]] && [[ "$bitness" == "64" ]]; then
+	if [[ "$var" == "fake08sa" ]]  && [[ "$bitness" == "64" ]]; then
 	 cd $cur_wd
 
 	  # Now we'll start the clone and build of fake08
-	  if [ ! -d "fake-08/" ]; then
-		git clone --recursive https://github.com/jtothebell/fake-08.git
+	  if [ ! -d "fake-08sa/" ]; then
+		git clone --recursive https://github.com/jtothebell/fake-08.git -d fake-08sa
 
 		if [[ $? != "0" ]]; then
 		  echo " "
 		  echo "There was an error while cloning the fake08 standalone git.  Is Internet active or did the git location change?  Stopping here."
 		  exit 1
 		fi
-		cp patches/fake08-patch* fake-08/.
+		cp patches/fake08sa-patch* fake-08sa/.
 	  else
 		echo " "
-		echo "A fake-08 subfolder already exists.  Stopping here to not impact anything in the folder that may be needed.  If not needed, please remove the fake-08 folder and rerun this script."
+		echo "A fake-08sa subfolder already exists.  Stopping here to not impact anything in the folder that may be needed.  If not needed, please remove the fake-08 folder and rerun this script."
 		echo " "
 		exit 1
 	  fi
 
-	 cd fake-08
+	 cd fake-08sa
 
-	 fake08_patches=$(find *.patch)
+	 fake08sa_patches=$(find *.patch)
 
-	 if [[ ! -z "$fake08_patches" ]]; then
-	  for patching in fake08-patch*
+	 if [[ ! -z "$fake08sa_patches" ]]; then
+	  for patching in fake08sa-patch*
 	  do
 		   patch -Np1 < "$patching"
 		   if [[ $? != "0" ]]; then
@@ -50,25 +50,22 @@ bitness="$(getconf LONG_BIT)"
 	  done
 	  fi
 
-           make -C platform/libretro clean
-           make -C platform/libretro -j$(nproc)
+           make clean-sdl2
+           make sdl2 -j$(nproc)
            if [[ $? != "0" ]]; then
 		     echo " "
 		     echo "There was an error that occured while making the fake08 standalone.  Stopping here."
              exit 1
            fi
-           strip platform/libretro/fake08_libretro.so
+           strip platform/SDL2Desktop/FAKE08
 
-	       if [ ! -d "../cores64/" ]; then
-		     mkdir -v ../cores64
-	       fi
+           if [ ! -d "../fake08sa-$bitness/" ]; then
+	         mkdir -v ../fake08sa-$bitness
+           fi
 
-	       cp platform/libretro/fake08_libretro.so ../cores64/.
+	   cp platform/SDL2Desktop/FAKE08 ../fake08sa-$bitness/fake08
 
-	       gitcommit=$(git log | grep -m 1 commit | cut -c -14 | cut -c 8-)
-	       echo $gitcommit > ../cores$(getconf LONG_BIT)/fake08_libretro.so.commit
-
-	       echo " "
-	       echo "The fake08 executable has been created and has been placed in the rk3326_core_builds/cores64 subfolder"
+	   echo " "
+	   echo "The fake08 executable has been created and has been placed in the rk3326_core_builds/fake08sa-$bitness subfolder"
 
 	fi
