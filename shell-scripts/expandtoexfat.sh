34fdd42ae0581fc900f661d3fec88f60536af6da
christianhaitian <mckman@comcast.net>
2023-01-18 18:21:42 -0500

    Update expandtoexfat.sh

--- shell-scripts/expandtoexfat.sh
+++ shell-scripts/expandtoexfat.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 sudo umount /roms
+sudo ln -s /dev/mmcblk0 /dev/hda
 sudo ln -s /dev/mmcblk0p3 /dev/hda3
 sudo chmod 666 /dev/tty1
 export TERM=linux
 height="15"
 width="55"
-if [ -f "/boot/rk3326-rg351v-linux.dtb" ] || [ -f "/boot/rk3326-rg351mp-linux.dtb" ] || [ -f "/boot/rk3326-gameforce-linux.dtb" ] || [ -f "/boot/rk3326-odroidgo3-linux.dtb" ]; then
+if [ -f "/boot/rk3326-rg351v-linux.dtb" ] || [ -f "/boot/rk3326-rg351mp-linux.dtb" ] || [ -f "/boot/rk3326-gameforce-linux.dtb" ] || [ -f "/boot/rk3326-odroidgo3-linux.dtb" ] || [ -f "/boot/rk3566.dtb" ]; then
   sudo setfont /usr/share/consolefonts/Lat7-Terminus20x10.psf.gz
   height="20"
   width="60"
 fi
-[ ! -f /boot/doneit ] && { sudo echo ", +" | sudo sfdisk -N 3 --force /dev/mmcblk0; sudo touch "/boot/doneit"; dialog --infobox "EASYROMS partition expansion and conversion to exfat in process.  Please be patient as the system will reboot 2 times to complete this." $height $width 2>&1 > /dev/tty1 | sleep 10; reboot; }
+
+if [ ! -f /boot/doneit ]; then
+  sudo echo ", +" | sudo sfdisk -N 3 --force /dev/mmcblk0
+  sudo touch "/boot/doneit"
+  dialog --infobox "EASYROMS partition expansion and conversion to exfat in process.  The device will now reboot to continue the process..." $height $width 2>&1 > /dev/tty1
+  sudo reboot
+fi
+
+maxSize=$(lsblk -b --output SIZE -n -d /dev/mmcblk0)
+
+newExtSizePct=$(printf %.2f "$((10**4 * 9589934592/$maxSize))e-4")
+newExtSizePct=$(echo print 1-$newExtSizePct | perl)
+ExfatPctToRemain=$(echo print 100*$newExtSizePct | perl)
+
+#echo "$ExfatPctToRemain" > /home/ark/growpercentage.log
+
+# Expand the ext4 partition if possible to make room for future update needs
+if [ $ExfatPctToRemain -lt "100" ]; then
+  printf "d\n3\nw\nq\n" | sudo fdisk /dev/mmcblk0
+  sudo growpart --free-percent=$ExfatPctToRemain -v /dev/mmcblk1 2
+  sudo resize2fs /dev/mmcblk0p2
+  printf "n\n3\n\n\ny\nw\n" | sudo fdisk /dev/mmcblk0
+fi
+
 sudo mkfs.exfat -s 16K -n EASYROMS /dev/hda3
 exitcode=$?
 sync
@@ -25,16 +49,15 @@ sync
 reqSpace=1000000
 availSpace=$(df "/roms" | awk 'NR==2 { print $4 }')
 if (( availSpace < reqSpace )); then
-  sudo rm -rf -v /tempthemes/es-theme-epic-cody*/
+  sudo rm -rf -v /tempthemes/es-theme-epic-cody-RG351V/
 fi
 sudo rm -rf -v /roms/themes/es-theme-nes-box/
 # Setup swapfile
-printf "\n\n\e[32mSetting up swapfile.  Please wait...\n"
-printf "\033[0m"
-sudo dd if=/dev/zero of=/swapfile bs=1024 count=262144
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+#printf "\n\n\e[32mSetting up swapfile.  Please wait...\n"
+#printf "\033[0m"
+#sudo dd if=/dev/zero of=/swapfile bs=1024 count=262144
+#sudo mkswap /swapfile
+#sudo swapon /swapfile
 sudo mv -f -v /tempthemes/* /roms/themes
 sync
 sleep 1
@@ -49,16 +72,16 @@ if [ ! -f "/boot/rk3326-rg351v-linux.dtb" ] && [ ! -f "/boot/rk3326-rg351mp-linu
 fi
 sudo rm -f /boot/fstab.exfat
 # Disable and delete swapfile
-sudo swapoff /swapfile
-sudo rm -f -v /swapfile
+#sudo swapoff /swapfile
+#sudo rm -f -v /swapfile
 if [ $exitcode -eq 0 ]; then
-  dialog --infobox "Completed expansion of EASYROMS partition and conversion to exfat. System will reboot and load Emulationstation in a few seconds." $height $width 2>&1 > /dev/tty1 | sleep 10
+  dialog --infobox "Completed expansion of EASYROMS partition and conversion to exfat. The system will now reboot and load ArkOS." $height $width 2>&1 > /dev/tty1 | sleep 10
   systemctl disable firstboot.service
   sudo rm -v /boot/firstboot.sh
   sudo rm -v -- "$0"
   reboot
 else
-  dialog --infobox "EASYROMS partition expansion and conversion to exfat failed for an unknown reason.  Please expand the partition using an alternative tool such as Minitool Partition Wizard.  System will reboot and load Emulationstation now." $height $width 2>&1 > /dev/tty1 | sleep 10
+  dialog --infobox "EASYROMS partition expansion and conversion to exfat failed for an unknown reason.  Please expand the partition using an alternative tool such as Minitool Partition Wizard.  System will reboot and load ArkOS now." $height $width 2>&1 > /dev/tty1 | sleep 10
   systemctl disable firstboot.service
   sudo rm -v /boot/firstboot.sh
   sudo rm -v -- "$0"
