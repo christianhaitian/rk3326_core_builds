9a81c79c3f38130716017fc10d0e6fb803d39d9a
christianhaitian <mckman@comcast.net>
2023-01-27 23:26:22 -0500

    Update importwifi.sh

--- shell-scripts/importwifi.sh
+++ shell-scripts/importwifi.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [[ -f "/opt/system/Tools/wifikeyfile.txt" ]]; then
 
+if [[ -f $(find /opt/system/Tools -maxdepth 1 -iname wifikeyfile.txt) ]]; then
+
+	keyfile=`find /opt/system/Tools -maxdepth 1 -iname wifikeyfile.txt`
+	keyfile_base=`basename $keyfile`
 	sudo chmod 666 /dev/tty1
 	export TERM=linux
 	export XDG_RUNTIME_DIR=/run/user/$UID/
@@ -39,50 +42,46 @@ if [[ -f "/opt/system/Tools/wifikeyfile.txt" ]]; then
 	dialog --infobox "Starting Wifi importer.  Please wait..." 5 $width > /dev/tty1
 	#printf "Starting Wifi importer.  Please wait..." > /dev/tty1
 	sudo systemctl stop networkwatchdaemon
-	sudo systemctl start NetworkManager
+	sudo systemctl restart NetworkManager
 
-	mapfile wificreds < /opt/system/Tools/wifikeyfile.txt
+	dos2unix "$keyfile"
+	mapfile wificreds < "$keyfile"
 	ssid=$(echo ${wificreds[0]} | grep -oP '(?<=").*?(?=")')
 	pass=$(echo ${wificreds[1]} | grep -oP '(?<=").*?(?=")')
 
 	if [ ! -z "$ssid" ] && [ ! -z "$pass" ]; then
-	  dialog --infobox "\nConnecting to Wi-Fi $ssid ..." 5 $width > /dev/tty1
-	  clist=`sudo nmcli -f ALL --mode tabular --terse --fields IN-USE,SSID,CHAN,SIGNAL,SECURITY dev wifi`
-	  WPA3=`echo "$clist" | grep "$ssid" | grep " WPA3"`
+	  dialog --infobox "\nConnecting to $ssid ..." 5 $width > /dev/tty1
 
 	  # try to connect
-	  output=`nmcli con delete "$ssid"`
-	  if [[ "$WPA3" != *" WPA3"* ]]; then
-		output=`nmcli device wifi connect "$ssid" password "$pass"`
-	  else
-		#workaround for wpa2/wpa3 connectivity
-		output=`nmcli device wifi connect "$ssid" password "$pass"`
-		sudo sed -i '/key-mgmt\=sae/s//key-mgmt\=wpa-psk/' /etc/NetworkManager/system-connections/"$ssid".nmconnection
-		sudo systemctl restart NetworkManager
-		output=`nmcli con up "$ssid"`
-	  fi
+	  sudo nmcli con delete "$ssid" > /dev/null
+	  #workaround for wpa2/wpa3 connectivity
+	  sudo nmcli c add con-name "$ssid" type wifi ssid "$ssid" ifname wlan0 > /dev/null
+	  sudo nmcli c modify "$ssid" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "$pass" > /dev/null
+	  output=`sudo nmcli con up "$ssid"`
+
 	  success=`echo "$output" | grep successfully`
 
 	  if [ -z "$success" ]; then
-		output="Activation failed: File has been renamed to wifikeyfile.txt.nogood in the tools folder.  Please review it, correct it, rename it back to wifikeyfile.txt and try again."
-		cp -f /opt/system/Tools/wifikeyfile.txt /opt/system/Tools/wifikeyfile.txt.nogood
-		sudo rm -f /opt/system/Tools/wifikeyfile.txt
+		output="Activation failed: File has been renamed to ${keyfile_base}.nogood in the tools folder.  Please review it, correct it, rename it back to wifikeyfile.txt and try again."
+		sudo nmcli con delete "$ssid" > /dev/null
+		cp -f "$keyfile" "$keyfile".nogood
+		rm -f "$keyfile"
 	  else
-		sudo rm -f /opt/system/Tools/wifikeyfile.txt
-		output="Device successfully activated and connected to Wi-Fi.  wifikeyfile.txt has been deleted for security."
+		rm -f "$keyfile"
+		output="Device successfully activated and connected to $ssid.\n\n${keyfile_base} has been deleted for security purposes."
 	  fi
 
-	  dialog --infobox "\n$output" 6 $width > /dev/tty1
+	  dialog --infobox "\n$output" 8 $width > /dev/tty1
 	  sleep 10
 	else
-	  cp -f /opt/system/Tools/wifikeyfile.txt /opt/system/Tools/wifikeyfile.txt.nogood
-	  sudo rm -f /opt/system/Tools/wifikeyfile.txt
-	  dialog --infobox "The contents of the wifikeyfile.txt are invalid.  Nothing can be imported at this time.  File has been renamed to wifikeyfile.txt.nogood in the tools folder." $height $width 2>&1 > /dev/tty1 
+	  cp -f "$keyfile" "$keyfile".nogood
+	  rm -f "$keyfile"
+	  dialog --infobox "The contents of the ${keyfile_base} are invalid.  It can not be imported.  File has been renamed to ${keyfile}.nogood in the tools folder." $height $width 2>&1 > /dev/tty1 
 	  sleep 10
 	fi
 	sudo systemctl stop NetworkManager
 	sudo systemctl start networkwatchdaemon
-    printf "\033c" > /dev/tty1
+	printf "\033c" > /dev/tty1
 	exit 0
 fi
 
