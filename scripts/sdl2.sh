912971597a14d34235cba50ed48cb0a536310221
Christian_Haitian <mckman@comcast.net>
2023-02-06 00:48:20 +0000

    Updated sdl2 build script and patches

--- scripts/sdl2.sh
+++ scripts/sdl2.sh
@@ -12,7 +12,9 @@
 cur_wd="$PWD"
 bitness="$(getconf LONG_BIT)"
 #commit="25f9ed87ff6947d9576fc9d79dee0784e638ac58" # SDL 2.0.16
-commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it builds as 2.0.18.2 ¯\_(ツ)_/¯
+#commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it builds as 2.0.20.0 ¯\_(ツ)_/¯
+commit="f070c83a6059c604cbd098680ddaee391b0a7341" # SDL 2.0.26.2
+#commit="8c9beb0c873f6ca5efbd88f1ad2648bfc793b2ac" # SDL 2.0.24.0
 
 	# sdl2 Standalone Build
 	if [[ "$var" == "sdl2" ]]; then
@@ -59,6 +61,8 @@ commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it build
 	  done
 	 fi
 
+     #sed -i "s| -lrga||g" CMakeLists.txt
+
         if [[ $bitness == "32" ]]; then
          ./autogen.sh
          ./configure --host=arm-linux-gnueabihf \
@@ -71,51 +75,46 @@ commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it build
          mkdir build
          cd build
          cmake -DSDL_STATIC=OFF \
-               -DLIBC=ON \
-               -DGCC_ATOMICS=ON \
-               -DALTIVEC=OFF \
-               -DOSS=OFF \
-               -DALSA=ON \
-               -DALSA_SHARED=ON \
-               -DJACK=OFF \
-               -DJACK_SHARED=OFF \
-               -DESD=OFF \
-               -DESD_SHARED=OFF \
-               -DARTS=OFF \
-               -DARTS_SHARED=OFF \
-               -DNAS=OFF \
-               -DNAS_SHARED=OFF \
-               -DLIBSAMPLERATE=ON \
-               -DLIBSAMPLERATE_SHARED=ON \
-               -DSNDIO=OFF \
-               -DDISKAUDIO=OFF \
-               -DDUMMYAUDIO=OFF \
-               -DVIDEO_WAYLAND=OFF \
-               -DVIDEO_WAYLAND_QT_TOUCH=ON \
-               -DWAYLAND_SHARED=OFF \
-               -DVIDEO_MIR=OFF \
-               -DMIR_SHARED=OFF \
-               -DVIDEO_COCOA=OFF \
-               -DVIDEO_DIRECTFB=OFF \
-               -DVIDEO_VIVANTE=OFF \
-               -DDIRECTFB_SHARED=OFF \
-               -DFUSIONSOUND=OFF \
-               -DFUSIONSOUND_SHARED=OFF \
-               -DVIDEO_DUMMY=OFF \
-               -DINPUT_TSLIB=OFF \
-               -DPTHREADS=ON \
-               -DPTHREADS_SEM=ON \
-               -DDIRECTX=OFF \
-               -DSDL_DLOPEN=ON \
-               -DCLOCK_GETTIME=OFF \
-               -DRPATH=OFF \
-               -DRENDER_D3D=OFF \
-               -DVIDEO_X11=OFF \
-               -DVIDEO_OPENGL=OFF \
-               -DVIDEO_OPENGLES=ON \
-               -DVIDEO_VULKAN=OFF \
-               -DVIDEO_KMSDRM=ON \
-               -DPULSEAUDIO=ON ..
+			   -DSDL_LIBC=ON \
+			   -DSDL_GCC_ATOMICS=ON \
+			   -DSDL_ALTIVEC=OFF \
+			   -DSDL_OSS=OFF \
+			   -DSDL_ALSA=ON \
+			   -DSDL_ALSA_SHARED=ON \
+			   -DSDL_JACK=OFF \
+			   -DSDL_JACK_SHARED=OFF \
+			   -DSDL_ESD=OFF \
+			   -DSDL_ESD_SHARED=OFF \
+			   -DSDL_ARTS=OFF \
+			   -DSDL_ARTS_SHARED=OFF \
+			   -DSDL_NAS=OFF \
+			   -DSDL_NAS_SHARED=OFF \
+			   -DSDL_LIBSAMPLERATE=ON \
+			   -DSDL_LIBSAMPLERATE_SHARED=OFF \
+			   -DSDL_SNDIO=OFF \
+			   -DSDL_DISKAUDIO=OFF \
+			   -DSDL_DUMMYAUDIO=OFF \
+			   -DSDL_WAYLAND=OFF \
+			   -DSDL_WAYLAND_QT_TOUCH=OFF \
+			   -DSDL_WAYLAND_SHARED=OFF \
+			   -DSDL_COCOA=OFF \
+			   -DSDL_DIRECTFB=OFF \
+			   -DSDL_VIVANTE=OFF \
+			   -DSDL_DIRECTFB_SHARED=OFF \
+			   -DSDL_FUSIONSOUND=OFF \
+			   -DSDL_FUSIONSOUND_SHARED=OFF \
+			   -DSDL_DUMMYVIDEO=OFF \
+			   -DSDL_PTHREADS=ON \
+			   -DSDL_PTHREADS_SEM=ON \
+			   -DSDL_DIRECTX=OFF \
+			   -DSDL_CLOCK_GETTIME=OFF \
+			   -DSDL_RPATH=OFF \
+			   -DSDL_RENDER_D3D=OFF \
+			   -DSDL_X11=OFF \
+			   -DSDL_OPENGLES=ON \
+			   -DSDL_VULKAN=OFF \
+			   -DSDL_KMSDRM=ON \
+			   -DSDL_PULSEAUDIO=ON ..
           export LDFLAGS="${LDFLAGS} -lrga"
        fi
 
@@ -131,9 +130,9 @@ commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it build
 	  fi
 
       if [[ $bitness == "32" ]]; then
-	     strip build/.libs/libSDL2-2.0.so.0.18.2
+	     strip build/.libs/libSDL2-2.0.so.0.2600.2
 	  else
-	     strip libSDL2-2.0.so.0.18.2
+	     strip libSDL2-2.0.so.0.2600.2
 	  fi
 
 	  if [ ! -d "$cur_wd/sdl2-$bitness/" ]; then
@@ -141,9 +140,9 @@ commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it build
 	  fi
 
       if [[ $bitness == "32" ]]; then
-	     cp build/.libs/libSDL2-2.0.so.0.18.2 $cur_wd/sdl2-$bitness/.
+	     cp build/.libs/libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/.
 	  else
-	     cp libSDL2-2.0.so.0.18.2 $cur_wd/sdl2-$bitness/.
+	     cp libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/.
 	  fi
 
 	  echo " "
@@ -171,6 +170,7 @@ commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it build
        cd build
     fi
 
+      #make clean
 	  make -j$(nproc)
 
 	  if [[ $? != "0" ]]; then
@@ -180,11 +180,11 @@ commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it build
 	  fi
 
       if [[ $bitness == "32" ]]; then
-	     strip build/.libs/libSDL2-2.0.so.0.18.2
-	     cp build/.libs/libSDL2-2.0.so.0.18.2 $cur_wd/sdl2-$bitness/libSDL2-2.0.so.0.18.2.rotated
+	     strip build/.libs/libSDL2-2.0.so.0.2600.2
+	     cp build/.libs/libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/libSDL2-2.0.so.0.2600.2.rotated
 	  else
-	     strip libSDL2-2.0.so.0.18.2
-	     cp libSDL2-2.0.so.0.18.2 $cur_wd/sdl2-$bitness/libSDL2-2.0.so.0.18.2.rotated
+	     strip libSDL2-2.0.so.0.2600.2
+	     cp libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/libSDL2-2.0.so.0.2600.2.rotated
       fi
 
 	  echo " "
