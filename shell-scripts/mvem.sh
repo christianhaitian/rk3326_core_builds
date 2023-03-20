892ec379392f90cf0be1cfc1136fc304e2170cce
christianhaitian <mckman@comcast.net>
2023-03-20 15:18:21 -0400

    Update mvem.sh

--- shell-scripts/mvem.sh
+++ shell-scripts/mvem.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-res="480x320x1"
-if [ -f "/boot/rk3326-rg351v-linux.dtb" ]; then
-    res="640x480x1"
-elif [[ -e "/dev/input/by-path/platform-odroidgo3-joypad-event-joystick" ]]; then
-  res="854x480x1"
-  if [ -f "/boot/rk3326-rg351mp-linux.dtb" ]; then
-    res="640x480x1"
-  fi
-elif [[ -e "/dev/input/by-path/platform-singleadc-joypad-event-joystick" ]]; then
+if [[ -e "/dev/input/by-path/platform-singleadc-joypad-event-joystick" ]]; then
   xres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f1)"
   yres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f2)"
-  res="${xres}x${yres}x1"
 else
-  param_device="chi"
-  res="640x480x1"
+  xres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f2)"
+  yres="$(cat /sys/class/graphics/fb0/modes | grep -o -P '(?<=:).*(?=p-)' | cut -dx -f1)"
 fi
 
+res="${xres}x${yres}x1"
 directory="$(dirname "$1" | cut -d "/" -f2)"
 gamecontrols=$(echo "$(ls "$1" | cut -d "/" -f4 | cut -d "." -f1)")
+gamecontrols_nocase=$(find "/opt/mvem/controls" -maxdepth 1 -iname "${gamecontrols}".gptk)
+custom_gamecontrols_nocase=$(find "/$directory/mv/controls" -maxdepth 1 -iname "${gamecontrols}".gptk)
 
 cd /opt/mvem
 
@@ -26,15 +20,28 @@ sudo chmod 666 /dev/uinput
 
 export SDL_GAMECONTROLLERCONFIG_FILE="controls/gamecontrollerdb.txt"
 
-if [ -f "/$directory/mv/controls/${gamecontrols}.gptk" ]; then
-  echo "Loading custom user controls from /$directory/mv/controls/${gamecontrols}.gptk"
-  ./gptokeyb -1 "mvem" -c "/$directory/mv/controls/${gamecontrols}.gptk" &
-elif [ -f "/opt/mvem/controls/${gamecontrols}.gptk" ]; then
-  echo "Loading provided controls from /opt/mvem/controls/${gamecontrols}.gptk"
-  ./gptokeyb -1 "mvem" -c "/opt/mvem/controls/${gamecontrols}.gptk" &
+if [[ -e "/dev/input/by-path/platform-odroidgo2-joypad-event-joystick" ]]; then
+  if [ -f "$custom_gamecontrols_nocase" ]; then
+    echo "Loading custom user controls from $custom_gamecontrols_nocase"
+    LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.18.2" ./gptokeyb -1 "mvem" -c "$custom_gamecontrols_nocase" &
+  elif [ -f "$gamecontrols_nocase" ]; then
+    echo "Loading provided controls from $gamecontrols_nocase"
+    LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.18.2" ./gptokeyb -1 "mvem" -c "$gamecontrols_nocase" &
+  else
+    echo "Loading default controls /opt/mvem/controls/mvem.gptk"
+    LD_PRELOAD="/usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.18.2" ./gptokeyb -1 "mvem" -c "/opt/mvem/controls/mvem.gptk" &
+  fi
 else
-  echo "Loading default controls /opt/mvem/controls/mvem.gptk"
-  ./gptokeyb -1 "mvem" -c "/opt/mvem/controls/mvem.gptk" &
+  if [ -f "$custom_gamecontrols_nocase" ]; then
+    echo "Loading custom user controls from $custom_gamecontrols_nocase"
+    ./gptokeyb -1 "mvem" -c "$custom_gamecontrols_nocase" &
+  elif [ -f "$gamecontrols_nocase" ]; then
+    echo "Loading provided controls from $gamecontrols_nocase"
+    ./gptokeyb -1 "mvem" -c "$gamecontrols_nocase" &
+  else
+    echo "Loading default controls /opt/mvem/controls/mvem.gptk"
+    ./gptokeyb -1 "mvem" -c "/opt/mvem/controls/mvem.gptk" &
+  fi
 fi
 
 ./mvem "$1" "$res"
