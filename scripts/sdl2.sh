7881d8a5144b645bd00ed4c83e8ad6b41f2dc148
christianhaitian <mckman@comcast.net>
2023-04-25 14:38:24 -0400

    Update sdl2 script to build 2.26.5

--- scripts/sdl2.sh
+++ scripts/sdl2.sh
@@ -12,9 +12,9 @@
 cur_wd="$PWD"
 bitness="$(getconf LONG_BIT)"
 #commit="25f9ed87ff6947d9576fc9d79dee0784e638ac58" # SDL 2.0.16
-#commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it builds as 2.0.20.0 ¯\_(ツ)_/¯
-commit="f070c83a6059c604cbd098680ddaee391b0a7341" # SDL 2.0.26.2
-#commit="8c9beb0c873f6ca5efbd88f1ad2648bfc793b2ac" # SDL 2.0.24.0
+#commit="f9b918ff403782986f2a6712e6e2a462767a0457" # SDL 2.0.20 although it builds as 2.0.18.2 ¯\_(ツ)_/¯
+#commit="f070c83a6059c604cbd098680ddaee391b0a7341" # SDL 2.0.26.2
+commit="ac13ca9ab691e13e8eebe9684740ddcb0d716203" # SDL 2.0.26.5
 
 	# sdl2 Standalone Build
 	if [[ "$var" == "sdl2" ]]; then
@@ -63,60 +63,60 @@ commit="f070c83a6059c604cbd098680ddaee391b0a7341" # SDL 2.0.26.2
 
      #sed -i "s| -lrga||g" CMakeLists.txt
 
-        if [[ $bitness == "32" ]]; then
-         ./autogen.sh
-         ./configure --host=arm-linux-gnueabihf \
-         --enable-video-kmsdrm \
-         --disable-video-x11 \
-         --disable-video-rpi \
-         --disable-video-wayland \
-         --disable-video-vulkan
-       else
+        #if [[ $bitness == "32" ]]; then
+        # ./autogen.sh
+        # ./configure --host=arm-linux-gnueabihf \
+        # --enable-video-kmsdrm \
+        # --disable-video-x11 \
+        # --disable-video-rpi \
+        # --disable-video-wayland \
+        # --disable-video-vulkan
+       #else
          mkdir build
          cd build
          cmake -DSDL_STATIC=OFF \
-			   -DSDL_LIBC=ON \
-			   -DSDL_GCC_ATOMICS=ON \
-			   -DSDL_ALTIVEC=OFF \
-			   -DSDL_OSS=OFF \
-			   -DSDL_ALSA=ON \
-			   -DSDL_ALSA_SHARED=ON \
-			   -DSDL_JACK=OFF \
-			   -DSDL_JACK_SHARED=OFF \
-			   -DSDL_ESD=OFF \
-			   -DSDL_ESD_SHARED=OFF \
-			   -DSDL_ARTS=OFF \
-			   -DSDL_ARTS_SHARED=OFF \
-			   -DSDL_NAS=OFF \
-			   -DSDL_NAS_SHARED=OFF \
-			   -DSDL_LIBSAMPLERATE=ON \
-			   -DSDL_LIBSAMPLERATE_SHARED=OFF \
-			   -DSDL_SNDIO=OFF \
-			   -DSDL_DISKAUDIO=OFF \
-			   -DSDL_DUMMYAUDIO=OFF \
-			   -DSDL_WAYLAND=OFF \
-			   -DSDL_WAYLAND_QT_TOUCH=OFF \
-			   -DSDL_WAYLAND_SHARED=OFF \
-			   -DSDL_COCOA=OFF \
-			   -DSDL_DIRECTFB=OFF \
-			   -DSDL_VIVANTE=OFF \
-			   -DSDL_DIRECTFB_SHARED=OFF \
-			   -DSDL_FUSIONSOUND=OFF \
-			   -DSDL_FUSIONSOUND_SHARED=OFF \
-			   -DSDL_DUMMYVIDEO=OFF \
-			   -DSDL_PTHREADS=ON \
-			   -DSDL_PTHREADS_SEM=ON \
-			   -DSDL_DIRECTX=OFF \
-			   -DSDL_CLOCK_GETTIME=OFF \
-			   -DSDL_RPATH=OFF \
-			   -DSDL_RENDER_D3D=OFF \
-			   -DSDL_X11=OFF \
-			   -DSDL_OPENGLES=ON \
-			   -DSDL_VULKAN=OFF \
-			   -DSDL_KMSDRM=ON \
-			   -DSDL_PULSEAUDIO=ON ..
+               -DSDL_LIBC=ON \
+               -DSDL_GCC_ATOMICS=ON \
+               -DSDL_ALTIVEC=OFF \
+               -DSDL_OSS=OFF \
+               -DSDL_ALSA=ON \
+               -DSDL_ALSA_SHARED=ON \
+               -DSDL_JACK=OFF \
+               -DSDL_JACK_SHARED=OFF \
+               -DSDL_ESD=OFF \
+               -DSDL_ESD_SHARED=OFF \
+               -DSDL_ARTS=OFF \
+               -DSDL_ARTS_SHARED=OFF \
+               -DSDL_NAS=OFF \
+               -DSDL_NAS_SHARED=OFF \
+               -DSDL_LIBSAMPLERATE=ON \
+               -DSDL_LIBSAMPLERATE_SHARED=OFF \
+               -DSDL_SNDIO=OFF \
+               -DSDL_DISKAUDIO=OFF \
+               -DSDL_DUMMYAUDIO=OFF \
+               -DSDL_WAYLAND=OFF \
+               -DSDL_WAYLAND_QT_TOUCH=OFF \
+               -DSDL_WAYLAND_SHARED=OFF \
+               -DSDL_COCOA=OFF \
+               -DSDL_DIRECTFB=OFF \
+               -DSDL_VIVANTE=OFF \
+               -DSDL_DIRECTFB_SHARED=OFF \
+               -DSDL_FUSIONSOUND=OFF \
+               -DSDL_FUSIONSOUND_SHARED=OFF \
+               -DSDL_DUMMYVIDEO=OFF \
+               -DSDL_PTHREADS=ON \
+               -DSDL_PTHREADS_SEM=ON \
+               -DSDL_DIRECTX=OFF \
+               -DSDL_CLOCK_GETTIME=OFF \
+               -DSDL_RPATH=OFF \
+               -DSDL_RENDER_D3D=OFF \
+               -DSDL_X11=OFF \
+               -DSDL_OPENGLES=ON \
+               -DSDL_VULKAN=OFF \
+               -DSDL_KMSDRM=ON \
+               -DSDL_PULSEAUDIO=ON ..
           export LDFLAGS="${LDFLAGS} -lrga"
-       fi
+       #fi
 
       #../configure --prefix=$PWD/bin$bitness
 	  #make clean
@@ -129,64 +129,22 @@ commit="f070c83a6059c604cbd098680ddaee391b0a7341" # SDL 2.0.26.2
 		exit 1
 	  fi
 
-      if [[ $bitness == "32" ]]; then
-	     strip build/.libs/libSDL2-2.0.so.0.2600.2
-	  else
-	     strip libSDL2-2.0.so.0.2600.2
-	  fi
+      #if [[ $bitness == "32" ]]; then
+	  #   strip build/.libs/libSDL2-2.0.so.0.*
+	  #else
+	     strip libSDL2-2.0.so.0.*
+	  #fi
 
 	  if [ ! -d "$cur_wd/sdl2-$bitness/" ]; then
 		mkdir -v $cur_wd/sdl2-$bitness
 	  fi
 
-      if [[ $bitness == "32" ]]; then
-	     cp build/.libs/libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/.
-	  else
-	     cp libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/.
-	  fi
+      #if [[ $bitness == "32" ]]; then
+	  #   cp build/.libs/libSDL2-2.0.so.0.* $cur_wd/sdl2-$bitness/.
+	  #else
+	     cp libSDL2-2.0.so.0.* $cur_wd/sdl2-$bitness/.
+	  #fi
 
 	  echo " "
 	  echo "sdl $(git describe --tags | cut -c 9-) has been created and has been placed in the rk3326_core_builds/sdl2-$bitness subfolder"
 	fi
-
-    if [[ $bitness == "64" ]]; then
-       cd ..
-    fi
-
-    if [[ $sdl2_rotationpatch == "yes" ]]; then
-	  for patching in sdl2-patch*
-	  do
-	    patch -Np1 < "$patching"
-		if [[ $? != "0" ]]; then
-		  echo " "
-		  echo "There was an error while applying $patching.  Stopping here."
-		  exit 1
-		fi
-		rm "$patching"
-	  done
-	fi
-
-    if [[ $bitness == "64" ]]; then
-       cd build
-    fi
-
-      #make clean
-	  make -j$(nproc)
-
-	  if [[ $? != "0" ]]; then
-		echo " "
-		echo "There was an error while building sdl2 at commit $commit with the rotation patch applied.  Stopping here."
-		exit 1
-	  fi
-
-      if [[ $bitness == "32" ]]; then
-	     strip build/.libs/libSDL2-2.0.so.0.2600.2
-	     cp build/.libs/libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/libSDL2-2.0.so.0.2600.2.rotated
-	  else
-	     strip libSDL2-2.0.so.0.2600.2
-	     cp libSDL2-2.0.so.0.2600.2 $cur_wd/sdl2-$bitness/libSDL2-2.0.so.0.2600.2.rotated
-      fi
-
-	  echo " "
-	  echo "sdl $(git describe --tags | cut -c 9-) with rotation has been created and has been placed in the rk3326_core_builds/sdl2-$bitness subfolder"
-
