5af25804253840690f8ea01399051cdb274f8493
Christian_Haitian <mckman@comcast.net>
2024-09-29 15:13:42 +0000

    Revert SDL2.30.7 joystick recognition method commit e5024fae3decb724e397d3c9dbcb744d8c79aac1

--- scripts/sdl2.sh
+++ scripts/sdl2.sh
@@ -128,6 +128,7 @@ extension="3000.7"
 
       #../configure --prefix=$PWD/bin$bitness
 	  #make clean
+	  git revert -n e5024fae3decb724e397d3c9dbcb744d8c79aac1
 	  make -j$(nproc)
 	  #make install
 
