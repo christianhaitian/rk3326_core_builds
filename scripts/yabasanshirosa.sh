90b58e4f4f580d1501137d7e1588c4258c2fbe39
christianhaitian <mckman@comcast.net>
2025-10-24 22:42:18 -0400

    Update yabasanshirosa.sh to use TAG variable

--- scripts/yabasanshirosa.sh
+++ scripts/yabasanshirosa.sh
@@ -11,6 +11,7 @@
 
 cur_wd="$PWD"
 bitness="$(getconf LONG_BIT)"
+TAG="pi4-1-9-0"
 
 	# yabasanshiro Standalone build
 	if [[ "$var" == "yabasanshirosa" ]]; then
@@ -18,7 +19,7 @@ bitness="$(getconf LONG_BIT)"
 
 	  # Now we'll start the clone and build of yabasanshiro
 	  if [ ! -d "yabasanshiro/" ]; then
-		git clone --recursive https://github.com/devmiyax/yabause -b pi4-1-9-0 yabasanshiro
+		git clone --recursive https://github.com/devmiyax/yabause -b ${TAG} yabasanshiro
 
 		if [[ $? != "0" ]]; then
 		  echo " "
